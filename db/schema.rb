0be80a52c852987e9a1cbce6cffb1c27fb359957
rogerdunnGA <rogerdunn@Gmail.com>
2019-01-29 16:50:35 -0500

    end of Tues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_183648) do
+ActiveRecord::Schema.define(version: 2019_01_29_205531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,8 @@ ActiveRecord::Schema.define(version: 2019_01_29_183648) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +56,5 @@ ActiveRecord::Schema.define(version: 2019_01_29_183648) do
   end
 
   add_foreign_key "examples", "users"
+  add_foreign_key "jobs", "users"
 end
